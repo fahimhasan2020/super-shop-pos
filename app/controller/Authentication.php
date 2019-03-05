abd68fac2aad13922d7579866d4c96d4116e2ad4
fahim hasan <fahimhasan202020@gmail.com>
2019-03-05 13:33:48 +0600

    ui implement

---
+++ app/controller/Authentication.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fahim
+ * Date: 3/5/19
+ * Time: 1:16 PM
+ */
+
+namespace App;
+
+
+class Authentication
+{
+
+}
---
+++ app/controller/Pos.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fahim
+ * Date: 3/5/19
+ * Time: 1:17 PM
+ */
+
+namespace App;
+
+
+class Pos
+{
+
+}
